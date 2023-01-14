4984040cccee269bf579f74a8454fdd89405ca12
pitanihamsaraj <pitanihamsaraj@gmail.com>
2023-01-14 20:09:15 +0530

    feature/microbundle-setup (#1)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import LazyLoad from './LazyLoad';
-export default LazyLoad;
+import React, { useEffect, useRef, useState } from 'react'
+
+const LazyLoad = ({
+  threshold = 0.5,
+  height,
+  width,
+  style,
+  onVisible,
+  component,
+  children,
+  ...props
+}) => {
+  const [load, setLoad] = useState(false)
+  const ref = useRef()
+
+  const styleProps = {
+    ...((height || width || style) && { style: { height, width, ...style } })
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          observer.disconnect()
+          // eslint-disable-next-line no-unused-expressions
+          onVisible?.()
+          setLoad(true)
+        }
+      },
+      { threshold }
+    )
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+    return () => {
+      observer.disconnect()
+    }
+  }, [threshold])
+
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <div {...styleProps} {...props} ref={ref}>
+      {load ? component || children : null}
+    </div>
+  )
+}
+export default LazyLoad
